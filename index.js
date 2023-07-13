928ba80c0373595e7b4039f756807a65533b4a27
fatkurfaris <fatkurrizal12@gmail.com>
2023-07-14 03:56:07 +0700

    http-api-routing

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        // 'Content-Type': 'text/plain',
+        'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify({
+        id : 1,
+        name : 'Sir Issac Newton',
+    }));
+});
+
+server.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}...`);
+});    
