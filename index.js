7125400c3fcde146ce487ef95bcd4e45736dc08b
fatkurfaris <fatkurrizal12@gmail.com>
2023-07-14 04:59:16 +0700

    7-parameter-url

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+    {
+        id : 0,
+        name : 'Albeeerst einstein',
+    },
+    {
+        id : 1,
+        name : 'Nikola Tesla',
+    },
+    {
+        id : 2,
+        name : 'Sir Issac Newton',
+    }
+]
+
 server.on('request', (req, res) => {
-    if (req.url === 'friends') {
-        // res.writeHead(200, {
-        //     // 'Content-Type': 'text/plain',
-        //     'Content-Type': 'application/json',
-        // });
+    const items = req.url.split('/');
+
+    if (items[1] === 'friends') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            id : 1,
-            name : 'Sir Issac Newton',
-        }));
-    } else if (req.url === '/messages') {
+        if (items.length === 3){
+            const friendsIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendsIndex]))
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+        // res.end(JSON.stringify({
+        //     id : 1,
+        //     name : 'Sir Issac Newton',
+        // }));
+    } else if (items[1] === '/messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
